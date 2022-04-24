bd764b0af992b2ea5c99220568487ae5d9b280aa
KilliLogan <ivanov-p-a@bk.ru>
2022-04-24 17:43:50 +0300

    Tests

---
+++ app/src/test/java/pavel/ivanov/mvp_mvvm/AppTests.kt
@@ -0,0 +1,12 @@
+package pavel.ivanov.mvp_mvvm
+
+import org.junit.Assert
+import org.junit.Test
+
+class AppTests {
+
+    @Test
+    fun app_InstanceNotNull_ReturnTrue() {
+        Assert.assertNotNull(App)
+    }
+}
